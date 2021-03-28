f3b3ed6da484956cb9e3e6a5bde7ca250c456eed
ilham25 <ilhamadiputra4@gmail.com>
2021-03-28 14:29:39 +0700

    integrate with apollo graphql

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,20 @@ import Link from "next/link";
 import { Button, Row, Col, Container, Card } from "react-bootstrap";
 import Content from "../components/Content";
 
+import { gql } from "@apollo/client";
+import client from "../utils/apollo-client";
+
 import { dummyPost } from "../utils/data";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <>
       <Content>
-        {dummyPost.map((post, index) => (
+        {posts.map((post, index) => (
           <Col sm={12} key={index} className="mb-4">
             <Card>
-              <Card.Header>User : {post.user.fullName}</Card.Header>
+              <Card.Header>User : {post.userPost.fullName}</Card.Header>
               <Card.Body>
                 <Card.Title>{post.title}</Card.Title>
                 <Card.Text className="text-nowrap overflow-hidden text-truncate text-secondary">
@@ -28,4 +32,25 @@ export default function Home() {
       </Content>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query {
+        getAllPosts {
+          title
+          content
+          userPost {
+            fullName
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      posts: data.getAllPosts,
+    },
+  };
 }
